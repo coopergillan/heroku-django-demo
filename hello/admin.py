38265e53d995c9d228a6cfaa0cbbfa6701abd325
coopergillan <cooper.gillan@gmail.com>
2019-03-15 17:05:58 +0000

    Add admin portal for greetings

--- hello/admin.py
+++ hello/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from hello.models import Greeting
+
 # Register your models here.
+
+class GreetingAdmin(admin.ModelAdmin):
+    list_display = ('when', 'noun')
+    list_filter = ('noun', )
+    search_fields = ('noun', )
+
+admin.site.register(Greeting, GreetingAdmin)

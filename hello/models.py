528b329873b430c42911558309fb6a0f31c88c6a
coopergillan <cooper.gillan@gmail.com>
2019-03-15 16:55:07 +0000

    Add greeting noun and pick one when loading page.

--- hello/models.py
+++ hello/models.py
@@ -2,4 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Greeting(models.Model):
+    noun = models.TextField("greeting noun", default='Stranger')
     when = models.DateTimeField("date created", auto_now_add=True)
--- hello/views.py
+++ hello/views.py
@@ -1,10 +1,27 @@
 import os
+import random
 
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
 
 from .models import Greeting
+
+GREETING_NOUNS = (
+    'Friend', "Buddy ol' pal", 'Companion', 'Boon companion', 'Best friend',
+    'Close friend', 'Intimate', 'Confidante', 'Confidant', 'Familiar', 'Soul mate',
+    'Alter ego', 'Second self', 'Shadow', 'playmate', 'Playfellow',
+    'Classmate', 'Schoolmate', 'Workmate', 'Ally', 'Comrade', 'Associate',
+    'Sister', 'Brother', 'Pal', 'Bosom pal', 'Buddy', 'Bosom buddy', 'Chum',
+    'Spar', 'Sidekick', 'Cully', 'Crony', 'Main man', 'Bezzie', 'Mate', 'Oppo',
+    'China', 'Mucker', 'Butty', 'Bruvver', 'Bruv', 'Marrow', 'Marrer', 'Marra',
+    'Amigo', 'Compadre', 'Paisan', 'Homie', 'Bro', 'Homeboy', 'Homegirl',
+    'Gabba', 'Offsider',
+)
+
+def get_greeting_noun():
+    return random.choice(GREETING_NOUNS)
+
 
 # Create your views here.
 def index(request):
@@ -13,10 +30,9 @@ def index(request):
 
 
 def db(request):
-
-    greeting = Greeting()
+    greeting = Greeting(noun=get_greeting_noun())
     greeting.save()
 
     greetings = Greeting.objects.all()
 
-    return render(request, "db.html", {"greetings": greetings})
+    return render(request, "db.html", {"current_greeting": greeting, "greetings": greetings})
